a1b746b268e6fc4b62f75ba3c943119884a31f32
paulVer777 <pawel.gitara@onet.eu>
2018-04-11 23:20:03 +0200

    unnecesary class removed

--- jes.js
+++ jes.js
@@ -8,15 +8,15 @@ window.addEventListener("scroll", function (x) {
     if(pozycja>600) {
 
        $("#kox").fadeIn(500);
-        element.classList.remove("notvisible");
-        element.classList.add("visible");
+       /* element.classList.remove("notvisible");
+        element.classList.add("visible");*/
 
     }
     else if(pozycja<600)
     {
         $("#kox").fadeOut(300);
-        element.classList.remove("visible");
-        element.classList.add("notvisible");
+       /* element.classList.remove("visible");
+        element.classList.add("notvisible");*/
 
 
 
